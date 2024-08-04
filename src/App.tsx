c93129d9fbfb140b1327c41716236e82220b2ab4
samkc-0 <sam.k.c.0@icloud.com>
2024-08-04 16:11:26 +1200

    remove boilerplate css

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
 import { useState } from 'react'
-
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <h1>Counter</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
-  )
+  return <>Soon to be an R3F app</>
 }
 
 export default App
